5a362391c5234be19f560e27533b42f33631a0d9
peppe2794 <39173356+peppe2794@users.noreply.github.com>
2021-01-13 16:25:20 +0100

    Update maint.tf

--- maint.tf
+++ maint.tf
@@ -9,14 +9,35 @@ terraform {
 provider "proxmox" {
     pm_api_url = "https://10.224.16.41:8006/api2/json"
     pm_tls_insecure = "true"
+    pm_log_enable = "true"
+    pm_log_levels = {
+     _default = "debug"
+     _capturelog = ""
+    }
 }
 resource "proxmox_vm_qemu" "proxmox_vm" {
   count             = 1
   name              = "tf-vm"
   target_node       = "pve"
   clone             = "deployTemplate"
-  os_type           = "cloud-init"
-  memory            = 8192
+  preprovision = false
+  memory = 8192
+  cores = "4"
+  cpu = "kvm64"
+  full_clone = "false"
   pool = "Tesi_Zagaria"
+  define_connection_info = false
+  clone_wait = 30
+
+disk {
+  backup       = false
+  cache        = "none"
+  iothread     = false
+  replicate    = false
+  size         = "22732M"
+  slot         = 0
+  ssd          = false
+  storage      = "nas_storage"
+  type         = "scsi"
+}
  }
- 
